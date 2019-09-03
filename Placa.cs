3e188d469099cbe3069be1fa32b3fbf491d9ae03
ColdVolcano <mineflamecraft@hotmail.com>
2019-09-02 21:25:46 -0500

    Agregar archivos de proyecto.

---
+++ Placa.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace VerificaPlacas
+{
+    public class Placa : IComparable<Placa>
+    {
+        public readonly string Identificador;
+        public readonly int Lote;
+        public readonly int Numerando;
+
+        private readonly string _placa;
+
+        public static Placa VerificarPlacaJalisco(string placa)
+        {
+            string[] placaDividida;
+
+            if (!((placaDividida = placa.Split(new char[] { '-' }, StringSplitOptions.RemoveEmptyEntries)).Length == 3))
+                return null;
+
+            string inicial = placaDividida[0];
+            if (!(inicial.Length == 3 && inicial.All(c => char.IsUpper(c)) && (inicial[0] == 'H' || inicial[0] == 'J')))
+                return null;
+
+            string segundo = placaDividida[1];
+            if (!(segundo.Length == 2 && segundo.All(c => char.IsDigit(c))))
+                return null;
+
+            string tercero = placaDividida[2];
+            if (!(tercero.Length == 2 && tercero.All(c => char.IsDigit(c))))
+                return null;
+
+            return new Placa(placa);
+        }
+
+        private Placa(string placa)
+        {
+            _placa = placa;
+            Identificador = placa.Substring(0, 3);
+            Lote = int.Parse(placa.Substring(4, 2));
+            Numerando = int.Parse(placa.Substring(7, 2));
+        }
+        public override string ToString()
+        {
+            return _placa;
+        }
+        public int CompareTo(Placa other)
+        {
+            return _placa.CompareTo(other.ToString());
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace VerificaPlacas
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            SortedDictionary<string, int> contadores = new SortedDictionary<string, int>();
+            SortedList<Placa, string> todasPlacas = new SortedList<Placa, string>();
+
+            Console.WriteLine("Presione una tecla cuando tenga la consola en su mayor tamaño posible");
+            Console.ReadKey(true);
+            Console.Clear();
+
+            string[] lines = File.ReadAllLines("sample.txt");
+
+            Console.WriteLine("Placas validas:");
+
+            int longitudLinea = 0;
+
+            foreach (string line in lines)
+            {
+                Placa placa = Placa.VerificarPlacaJalisco(line);
+
+                if (placa == null || todasPlacas.Keys.IndexOf(placa) != -1)
+                    continue;
+
+                todasPlacas.Add(placa, null);
+
+                string lote = line.Substring(0, 6);
+
+                if (contadores.Keys.Any(c => c.Equals(lote)))
+                    contadores[lote]++;
+                else
+                    contadores.Add(lote, 1);
+
+                Console.Write(placa);
+                longitudLinea += 10;
+                if (Console.WindowWidth < longitudLinea + 10 || line == lines[lines.Length - 1])
+                {
+                    Console.WriteLine();
+                    longitudLinea = 0;
+                }
+                else
+                {
+                    Console.Write(' ');
+                }
+            }
+
+            Console.WriteLine("Presione una tecla para mostrar las placas ordenadas alfabeticamente");
+            Console.ReadKey(true);
+            Console.Clear();
+            Console.WriteLine("Placas ordenadas");
+
+            foreach (var placa in todasPlacas.Keys)
+            {
+                Console.Write(placa);
+                longitudLinea += 10;
+                if (Console.WindowWidth < longitudLinea + 10 || placa == todasPlacas.Keys.Last())
+                {
+                    Console.WriteLine();
+                    longitudLinea = 0;
+                }
+                else
+                {
+                    Console.Write(' ');
+                }
+            }
+
+            Console.WriteLine("Presione una tecla para mostrar las placas que existen por lote");
+            Console.ReadKey(true);
+            Console.Clear();
+            Console.WriteLine("Placas por lote");
+
+            foreach (var lote in contadores)
+            {
+                Console.Write(lote.Key + ' ' + lote.Value);
+                longitudLinea += 8 + lote.Value.ToString().Length;
+                if (Console.WindowWidth < longitudLinea + 10 || lote.Key == contadores.Last().Key)
+                {
+                    Console.WriteLine();
+                    longitudLinea = 0;
+                }
+                else
+                {
+                    Console.Write(' ');
+                }
+            }
+
+            Console.WriteLine("Presione una tecla para salir");
+            Console.ReadKey(true);
+        }
+    }
+}
